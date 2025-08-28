317d51c31dfd8627c226c02e695473a367bfe385
Ron-Gan <e1291569@u.nus.edu>
2025-08-28 17:54:07 +0800

    Changed name, added Greet & Exit lines.
    Changed name from Duke to DefinitelyRealRon. Greet, Exit and Horizontal lines added.

---
+++ src/main/java/DefinitelyRealRon.java
@@ -0,0 +1,13 @@
+public class DefinitelyRealRon {
+    public static void main(String[] args) {
+        String lineString = "____________________________________________________________";
+        String welcomeString = " Hello! I'm DefinitelyRealRon\n What can I do for you?\n";
+        String byeString = " Bye. Hope to see you again soon!";
+        System.out.println(lineString);
+        System.out.println(welcomeString);
+        System.out.println(lineString);
+        System.out.println(byeString);
+        System.out.println(lineString);
+
+    }
+}

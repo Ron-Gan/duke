2537b26d9c610f4da885585a4b868668a6926388
Ron-Gan <e1291569@u.nus.edu>
2025-08-29 11:07:19 +0800

    Added echo and line encapsulation function.
    Echo function will repeat user input back to them and program only ends when user inputs "bye".
    Line encapsulation function makes printing of statements easier.
    Added some personality to DefinitelyRealRon.

--- src/main/java/DefinitelyRealRon.java
+++ src/main/java/DefinitelyRealRon.java
@@ -1,13 +1,32 @@
-public class DefinitelyRealRon {
-    public static void main(String[] args) {
-        String lineString = "____________________________________________________________";
-        String welcomeString = " Hello! I'm DefinitelyRealRon\n What can I do for you?\n";
-        String byeString = " Bye. Hope to see you again soon!";
-        System.out.println(lineString);
-        System.out.println(welcomeString);
-        System.out.println(lineString);
-        System.out.println(byeString);
-        System.out.println(lineString);
+import java.util.Scanner;
 
+public class DefinitelyRealRon {
+    
+    private static final String LINE = "____________________________________________________________";
+
+    public static void printWithLine(String inputString){
+            System.out.println(LINE);
+            System.out.println(inputString);
+            System.out.println(LINE);
+    }
+     
+    public static void echo(){
+        Scanner in = new Scanner(System.in);
+        String inputString;
+        do{
+            inputString = in.nextLine();
+            printWithLine(" "+inputString);
+        }
+        while(!inputString.equals("bye"));
+        in.close();
+        return;
+    }
+    public static void main(String[] args) {
+        String welcomeString = " Yo! I'm DefinitelyRealRon. Definitely Real. Definitely Ron.\n How may I help you today?";
+        String byeString = " Seeya! Hope I was helpful. I'm Definitely Real!";
+        printWithLine(welcomeString);
+        echo();
+        System.out.println(byeString);
+        System.out.println(LINE);
     }
 }

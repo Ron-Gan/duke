534dafc6fa6b8ebf5c6e85a74b15ed713fc69f45
Ron-Gan <e1291569@u.nus.edu>
2025-08-29 12:20:05 +0800

    Completed List function.
    List function implemented using ArrayList.
    Currently still using ReadInput as one big function. May need to refactor later.

--- src/main/java/DefinitelyRealRon.java
+++ src/main/java/DefinitelyRealRon.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
-import java.util.Vector;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DefinitelyRealRon {
     
     private static final String LINE = "____________________________________________________________";
-    public static Vector<String> itemList = new Vector<String>();
+    public static List<String> itemList = new ArrayList<String>();
 
     public static void printWithLine(String inputString){
             System.out.println(LINE);
@@ -19,20 +20,30 @@ public class DefinitelyRealRon {
     public static void readInput(){
         Scanner in = new Scanner(System.in);
         String inputString;
-        do{
-            inputString = in.nextLine();
-            if(inputString.equals("list")){
+        inputString = in.nextLine();
+        String printListString = "";
 
+        while(!inputString.equals("bye")){ //does not end until user inputs "bye"
+            if(inputString.equals("list")){
+                for(int i=0; i<itemList.size(); i++){
+                    printListString = printListString + (i+1) + ". " + itemList.get(i) + "\n";
+                }
+                System.out.println(LINE);
+                System.out.print(printListString);
+                System.out.println(LINE);
+                printListString="";
             }
             else{
+                itemList.add(inputString);
                 echo(inputString);
             }
+            inputString = in.nextLine();
         }
-        while(!inputString.equals("bye"));
+        System.out.println(LINE);
     }
     public static void main(String[] args) {
         String welcomeString = " Yo! I'm DefinitelyRealRon. Definitely Real. Definitely Ron.\n How may I help you today?";
-        String byeString = " Seeya! Hope I was helpful. I'm Definitely Real!";
+        String byeString = " Seeya! Hope I was helpful. I'm DefinitelyRealRon!";
         printWithLine(welcomeString);
         readInput();
         System.out.println(byeString);

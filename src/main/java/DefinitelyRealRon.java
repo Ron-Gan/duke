c4f667fa990f4575b16c0246506c8dfd9107721c
Ron-Gan <e1291569@u.nus.edu>
2025-08-29 11:48:27 +0800

    Detached ReadInput from Echo function.
    ReadInput is now a separate function that will call echo if needed.
    Added "added: " to echo output.

--- src/main/java/DefinitelyRealRon.java
+++ src/main/java/DefinitelyRealRon.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+import java.util.Vector;
 
 public class DefinitelyRealRon {
     
     private static final String LINE = "____________________________________________________________";
+    public static Vector<String> itemList = new Vector<String>();
 
     public static void printWithLine(String inputString){
             System.out.println(LINE);
@@ -10,22 +12,29 @@ public class DefinitelyRealRon {
             System.out.println(LINE);
     }
      
-    public static void echo(){
+    public static void echo(String inputString){
+        printWithLine(" added: "+inputString);
+    }
+
+    public static void readInput(){
         Scanner in = new Scanner(System.in);
         String inputString;
         do{
             inputString = in.nextLine();
-            printWithLine(" "+inputString);
+            if(inputString.equals("list")){
+
+            }
+            else{
+                echo(inputString);
+            }
         }
         while(!inputString.equals("bye"));
-        in.close();
-        return;
     }
     public static void main(String[] args) {
         String welcomeString = " Yo! I'm DefinitelyRealRon. Definitely Real. Definitely Ron.\n How may I help you today?";
         String byeString = " Seeya! Hope I was helpful. I'm Definitely Real!";
         printWithLine(welcomeString);
-        echo();
+        readInput();
         System.out.println(byeString);
         System.out.println(LINE);
     }

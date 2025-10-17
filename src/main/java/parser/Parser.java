dc1dc006ecf5f35de23d4e79ffdf39e8dc7095fa
Ron-Gan <e1291569@u.nus.edu>
2025-10-18 01:45:43 +0800

    Updated Messages and Parser with JavaDoc.

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -12,11 +12,18 @@ import java.time.format.DateTimeParseException;
 
 import commands.*;
 
+/**
+ * Makes sense of the user's input.
+ */
 public class Parser {
     public Command parse(String input){
+        /** Search for the first word as the commandWord. */
         String commandWord = input.split(" ")[0];
+
+        /** All other words other than the first will be the arguments. */
         String arguments = input.substring(commandWord.length()).strip();
 
+        /** Read the input differently according to the commandWord. */
         switch(commandWord){
             case "todo":
                 return(prepareAddTodo(arguments));
@@ -37,12 +44,28 @@ public class Parser {
         }
     }
 
+    /**
+     * Parses arguments in the context of the AddTodoCommand.
+     * Returns error if arguments are blank.
+     * @param arguments input except for first word
+     * @return the prepared command
+     */
     private Command prepareAddTodo(String arguments){
         if(arguments.isBlank()){
             return new ErrorCommand(ERROR_EMPTY_DESC);
         }
         return new AddTodoCommand(arguments);
     }
+
+    
+    /**
+     * Parses arguments in the context of the AddDeadlineCommand.
+     * Returns error if arguments are blank.
+     * Returns error if /by is missing.
+     * Returns error if deadline is not formatted in dd/MM/yyyy HHmm
+     * @param arguments input except for first word
+     * @return the prepared command
+     */
     private Command prepareAddDeadline(String arguments){
         if(!arguments.contains(" /by ")){
             return new ErrorCommand(ERROR_INVALID_BY);
@@ -61,6 +84,14 @@ public class Parser {
         }
     }
     
+    /**
+     * Parses arguments in the context of the AddEventCommand.
+     * Returns error if arguments are blank.
+     * Returns error if /from or /to is missing.
+     * Returns error if deadline is not formatted in dd/MM/yyyy HHmm
+     * @param arguments input except for first word
+     * @return the prepared command
+     */
     private Command prepareAddEvent(String arguments){
         boolean fromExist = (arguments.contains("/from "));
         boolean toExist = (arguments.contains("/to "));
@@ -70,16 +101,24 @@ public class Parser {
             String missing = !fromExist ? "/from" : "/to";
             return new ErrorCommand(String.format(ERROR_INVALID_FROM_TO, missing));
         }
+
+        /** 
+         * Determines if user input /from or /to first.
+         * Runs the first way of parsing if /from is before /to.
+         */
         if(arguments.indexOf("/from ")<arguments.indexOf("/to ")){
             fromDate = arguments.substring(arguments.indexOf("/from ")+6,arguments.indexOf("/to ")-1);
             toDate = arguments.substring(arguments.indexOf("/to ")+4);
             description = arguments.split(" /from ")[0];
         }
+
+        /** Runs if the way of parsing is /to before /from. */
         else{
             toDate=arguments.substring(arguments.indexOf("/to ")+4,arguments.indexOf("/from ")-1);
             fromDate = arguments.substring(arguments.indexOf("/from ")+6);
             description = arguments.split(" /to ")[0];
         } 
+
         if (description.isBlank()) {
             return new ErrorCommand(ERROR_EMPTY_DESC);
         }
@@ -94,6 +133,14 @@ public class Parser {
     private Command prepareList(){
         return new ListCommand();
     }
+
+    /**
+     * Parses arguments in the context of the MarkCommand.
+     * Returns error if arguments are blank.
+     * Returns error if target index is out of bounds.
+     * @param arguments input except for first word.
+     * @return the prepared command.
+     */
     private Command prepareMark(String arguments){
         Integer index=-1;
         if(arguments.isBlank()){
@@ -106,6 +153,14 @@ public class Parser {
         }
         return new MarkCommand(index);
     }
+
+    /**
+     * Parses arguments in the context of the UnmarkCommand.
+     * Returns error if arguments are blank.
+     * Returns error if target index is out of bounds.
+     * @param arguments input except for first word.
+     * @return the prepared command.
+     */
     private Command prepareUnmark(String arguments){
         Integer index = -1;
         if(arguments.isBlank()){
@@ -119,6 +174,13 @@ public class Parser {
         return new UnmarkCommand(index);
     }    
 
+    /**
+     * Parses arguments in the context of the MarkCommand.
+     * Returns error if arguments are blank.
+     * Returns error if target index is out of bounds.
+     * @param arguments input except for first word.
+     * @return the prepared command.
+     */
     private Command prepareDelete(String arguments){
         Integer index = -1;
         if(arguments.isBlank()){
